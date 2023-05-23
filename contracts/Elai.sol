ba299dcb09845bfd398dcf7e8c24b7df5c8d000c
EmoliftAI <132694728+EmoliftAI@users.noreply.github.com>
2023-05-23 23:05:40 +0200

    Add functions (with tests) : lock , burn , transfer and transferFrom. (#1)

--- contracts/Elai.sol
+++ contracts/Elai.sol
@@ -2,9 +2,50 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EmoLiftAI is ERC20 {
+contract EmoLiftAI is ERC20, Ownable {
+
+    // lockTime stores the timestamp until which an account's tokens are locked
+    // locked tokens cannot be transferred
+    mapping(address => uint256) public lockTime;
+
+    // constructor mints 1 billion tokens and assigns them to the owner of the contract
     constructor() ERC20("EmoLiftAI", "ELAI") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
+
+    // The lock function locks the tokens of a specified account for a specified amount of time
+    // Only the owner of the contract can lock tokens
+    function lock(address account, uint256 time) public onlyOwner {
+        // the lock time is the current time plus the specified amount of time
+        lockTime[account] = block.timestamp + time;
+    }
+
+    // The burn function burns a specified amount of tokens from a specified account
+    // Only the owner of the contract can burn tokens
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+
+    // The transfer function transfers a specified amount of tokens to a specified account
+    // The transfer will fail if the tokens are locked
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        // require that the current time is greater than the lock time
+        require(block.timestamp > lockTime[msg.sender], "Tokens are locked");
+
+        // call the transfer function of the ERC20 contract
+        return super.transfer(recipient, amount);
+    }
+
+    // The transferFrom function transfers a specified amount of tokens from one account to another
+    // The transfer will fail if the tokens are locked
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        // require that the current time is greater than the lock time
+        require(block.timestamp > lockTime[sender], "Tokens are locked");
+
+        // call the transferFrom function of the ERC20 contract
+        return super.transferFrom(sender, recipient, amount);
+    }
+    
 }
--- test/Elai.ts
+++ test/Elai.ts
@@ -1,52 +1,117 @@
-import { ethers } from "hardhat";
-import { expect } from "chai";
-import { Contract, ContractFactory } from "ethers";
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { Contract, Signer, ContractFactory } from 'ethers';
 
-describe("EmoLiftAI Contract", function () {
-    let EmoLiftAI: ContractFactory;
-    let elai: Contract;
+describe('EmoLiftAI', function () {
 
+  // Variables that will be used across tests
+  let EmoLiftAI: ContractFactory;
+  let emoLiftAI: Contract;
+  let owner: Signer, addr1: Signer, addr2: Signer, addrs: Signer[];
+
+  // This runs before each test, setting up a fresh contract for each test to use
   beforeEach(async () => {
-    EmoLiftAI = await ethers.getContractFactory("EmoLiftAI");
-    elai = await EmoLiftAI.deploy();
-    await elai.deployed();
+    // Get the contract factory
+    EmoLiftAI = await ethers.getContractFactory('EmoLiftAI');
+
+    // Get the signers (accounts that will interact with the contract)
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // Deploy the contract
+    emoLiftAI = await EmoLiftAI.deploy();
+    await emoLiftAI.deployed();
   });
 
-  it("should have correct name, symbol, and initial supply", async function () {
-    const name = await elai.name();
-    const symbol = await elai.symbol();
-    const totalSupply = await elai.totalSupply();
+  // Tests the deployment process
+  describe('Deployment', function () {
+    // Test if the owner is set correctly upon deployment
+    it('Should set the right owner', async function () {
+      expect(await emoLiftAI.owner()).to.equal(await owner.getAddress());
+    });
 
-    expect(name).to.equal("EmoLiftAI");
-    expect(symbol).to.equal("ELAI");
-    expect(totalSupply).to.equal(ethers.utils.parseEther("1000000000"));
+    // Test if the total supply is minted to the owner upon deployment
+    it('Should mint the total supply to owner', async function () {
+      const ownerBalance = await emoLiftAI.balanceOf(owner.getAddress());
+      const totalSupply = await emoLiftAI.totalSupply();
+      expect(ownerBalance.toString()).to.equal(totalSupply.toString());
+    });
   });
 
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+  // Tests token transactions
+  describe('Transactions', function () {
+    const amountToTransfer = ethers.utils.parseUnits('50', 'ether'); // 50 tokens
 
-    const EmoLiftAIFactory = await ethers.getContractFactory("EmoLiftAI");
-    const elai = await EmoLiftAIFactory.deploy();
-    await elai.deployed();
+    // Test if tokens can be transferred between accounts
+    it('Should transfer tokens between accounts', async function () {
+      const initialOwnerBalance = await emoLiftAI.balanceOf(owner.getAddress());
 
-    const ownerBalance = await elai.balanceOf(owner.address);
-    const totalSupply = await elai.totalSupply();
+      await emoLiftAI.transfer(addr1.getAddress(), amountToTransfer);
+      await emoLiftAI.transfer(addr2.getAddress(), amountToTransfer);
 
-    expect(ownerBalance).to.equal(totalSupply);
+      const finalOwnerBalance = await emoLiftAI.balanceOf(owner.getAddress());
+      expect(finalOwnerBalance.add(amountToTransfer.mul(2)).toString()).to.equal(initialOwnerBalance.toString());
+
+      const addr1Balance = await emoLiftAI.balanceOf(addr1.getAddress());
+      expect(addr1Balance.toString()).to.equal(amountToTransfer.toString());
+
+      const addr2Balance = await emoLiftAI.balanceOf(addr2.getAddress());
+      expect(addr2Balance.toString()).to.equal(amountToTransfer.toString());
+    });
+
+    // Test if transactions fail when a sender does not have enough tokens
+    it('Should fail if sender doesn’t have enough tokens', async function () {
+      const initialOwnerBalance = await emoLiftAI.balanceOf(owner.getAddress());
+    
+      await expect(
+        emoLiftAI.connect(addr1).transfer(owner.getAddress(), amountToTransfer)
+      ).to.be.revertedWith('ERC20: transfer amount exceeds balance');
+    
+      expect((await emoLiftAI.balanceOf(owner.getAddress())).toString()).to.equal(initialOwnerBalance.toString());
+    });
+    
   });
 
-  it("should transfer tokens correctly", async function () {
-    const [sender, recipient] = await ethers.getSigners();
+  // Tests token locking and burning
+  describe('Lock and Burn', function () {
+    const amountToTransfer = ethers.utils.parseUnits('50', 'ether'); // 50 tokens
+    const amountToBurn = ethers.utils.parseUnits('50', 'ether'); // 50 tokens
 
-    await elai.transfer(recipient.address, ethers.utils.parseEther("1000"));
+    // Test if the owner can burn tokens
+    it('Should allow owner to burn tokens', async function () {
+      const initialBalance = await emoLiftAI.balanceOf(owner.getAddress());
+      await emoLiftAI.burn(owner.getAddress(), amountToBurn);
+      const finalBalance = await emoLiftAI.balanceOf(owner.getAddress());
+      expect(finalBalance.add(amountToBurn).toString()).to.equal(initialBalance.toString());
+    });
 
-    const senderBalance = await elai.balanceOf(sender.address);
-    const recipientBalance = await elai.balanceOf(recipient.address);
+    // Test if non-owners are prevented from burning tokens
+    it('Should prevent non-owners from burning tokens', async function () {
+      await expect(emoLiftAI.connect(addr1).burn(addr1.getAddress(), amountToBurn)).to.be.revertedWith('Ownable: caller is not the owner');
+    });
 
-    expect(senderBalance).to.equal(ethers.utils.parseEther("999999000"));
-    expect(recipientBalance).to.equal(ethers.utils.parseEther("1000"));
+    // Test if tokens can be locked and then unlocked
+    it('Should lock and unlock tokens', async function () {
+      // Transfer some tokens to addr1
+      await emoLiftAI.transfer(addr1.getAddress(), amountToTransfer);
+    
+      // Lock addr1's tokens
+      await emoLiftAI.lock(addr1.getAddress(), 5000);
+    
+      // Verify lock
+      const lockedUntil = await emoLiftAI.lockTime(addr1.getAddress());
+      expect(lockedUntil.toNumber()).to.be.gt(Math.floor(Date.now() / 1000) + 5000);  // Date.now() is in milliseconds
+    
+      // Expect the transfer to fail due to locked tokens
+      await expect(emoLiftAI.connect(addr1).transfer(addr2.getAddress(), amountToTransfer))
+        .to.be.revertedWith('Tokens are locked');
+    
+      // Increase time and mine next block
+      await ethers.provider.send('evm_increaseTime', [6000]); // Increase by 6000 to account for potential round-off
+      await ethers.provider.send('evm_mine', []);
+    
+      // Now the transfer should succeed
+      await emoLiftAI.connect(addr1).transfer(addr2.getAddress(), amountToTransfer);
+      expect((await emoLiftAI.balanceOf(addr2.getAddress())).toString()).to.equal(amountToTransfer.toString());
+    });         
   });
-
-  // Add more test cases as needed
-
 });
